286a1edeab79d2f309ff0e1eef4106763fc93352
VladDrag <vdragomir@protonmail.com>
2022-11-26 21:36:41 +0100

    Add first xunit test. Get method test

--- CloudCustomers.API/Controllers/UsersController.cs
+++ CloudCustomers.API/Controllers/UsersController.cs
@@ -4,30 +4,18 @@ namespace CloudCustomers.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class UsersController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public UsersController()
     {
-        _logger = logger;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IActionResult> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return null;
+
     }
 }
 
---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,22 @@
+﻿using CloudCustomers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+
+public class TestUsersController
+{
+    [Fact]
+    public async Task Get_On_Success_Returns_Code_200()
+    {
+        //Arrange
+        var usersController = new UsersController();
+
+        //Act
+        var result = (OkObjectResult) await usersController.Get();
+
+        //Assert
+        result.StatusCode.Should().Be(200);
+    }
+}
